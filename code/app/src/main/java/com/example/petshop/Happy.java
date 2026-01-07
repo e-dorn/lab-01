6bcc29bd3fd486161489a39156aabbc846071030
e-dorn <ethanwdorn@gmail.com>
2026-01-07 10:00:53 -0700

    Lab demo done

---
+++ code/app/src/main/java/com/example/petshop/Happy.java
@@ -0,0 +1,21 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Happy extends Mood {
+
+    public Happy() {
+        super();
+    }
+    public Happy(Date date) {
+        super(date);
+    }
+
+    @Override
+    public String sayMood() {
+        return "yay";
+    }
+
+
+}
+
